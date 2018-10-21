e667532a8167b77455127958a19a192a1b9ce971
Giuseppe Guerra <to@nyo.zz.mu>
2018-10-21 23:23:41 +0200

    max 1 updater at the same time

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,17 +12,30 @@ namespace Updater
 {
     static class Program
     {
+        private static string guid = ((GuidAttribute)typeof(Program).Assembly.GetCustomAttributes(typeof(GuidAttribute), true)[0]).Value;
+
         /// <summary>
         /// Punto di ingresso principale dell'applicazione.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            if (Environment.OSVersion.Version.Major >= 6)
-                SetProcessDPIAware();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new UpdaterForm());
+            bool firstProcess = true;
+            using (Mutex mutex = new Mutex(true, guid, out firstProcess))
+            {
+                if (firstProcess)
+                {
+                    if (Environment.OSVersion.Version.Major >= 6)
+                        SetProcessDPIAware();
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new UpdaterForm());
+                }
+                else
+                {
+                    MessageBox.Show("The updater is already running.");
+                }
+            }
         }
 
         [System.Runtime.InteropServices.DllImport("user32.dll")]
