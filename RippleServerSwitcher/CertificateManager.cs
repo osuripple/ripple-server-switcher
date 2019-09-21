539f3a58f5965e79cd09e4632b8a73266653f14a
Maximilian Kruse <maxkruse1996@gmail.com>
2019-09-21 13:48:35 +0200

    Change how the certificate is being installed, hopefully this works lmao

--- RippleServerSwitcher/CertificateManager.cs
+++ RippleServerSwitcher/CertificateManager.cs
@@ -9,20 +9,18 @@ namespace RippleServerSwitcher
     {
         public string AuthoritySerialNumber
         {
-            get => Collection[0].GetSerialNumberString();
+            get => Cert.GetSerialNumberString();
         }
         public byte[] Certificate;
-        public X509Certificate2Collection Collection
+        public X509Certificate2 Cert
         {
             get
             {
-                X509Certificate2Collection collection = new X509Certificate2Collection();
-                collection.Import(Certificate);
-                return collection;
+                return X509Certificate2(Certificate);
             }
         }
 
-        private X509Certificate2Collection FindRippleCertificates(X509Store store)
+        private X509Certificate2Collection FindRippleCertificates(X509Certificate2 store)
         {
             // Too bad I can't subclass X509Store...
             return store.Certificates.Find(X509FindType.FindBySerialNumber, AuthoritySerialNumber, true);
@@ -51,19 +49,18 @@ namespace RippleServerSwitcher
                 if (FindRippleCertificates(store).Count > 0)
                     return;
 
-                foreach (X509Certificate2 cert in Collection)
-                    try
-                    {
-                        store.Add(cert);
-                    }
-                    catch (CryptographicException)
-                    {
-                        throw new HumanReadableException(
-                            "You must install the certificate.",
-                            "The certificate is needed to connect to Ripple through HTTPs. Without it, you won't be able to connect. " +
-                            "Please answer 'Yes' to the dialog window asking you to install the certificate in order to switch to Ripple."
-                        );
-                    }
+                try
+                {
+                    store.Add(Cert);
+                }
+                catch (CryptographicException)
+                {
+                    throw new HumanReadableException(
+                        "You must install the certificate.",
+                        "The certificate is needed to connect to Ripple through HTTPs. Without it, you won't be able to connect. " +
+                        "Please answer 'Yes' to the dialog window asking you to install the certificate in order to switch to Ripple."
+                    );
+                }
             }
             finally
             {
